734d606321be3fd2cc73e8e61b823bc61e262f76
vadim11041996 <vadim11041996@hotmail.com>
2018-06-07 17:50:55 +0300

    setup html

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ const morgan = require('morgan');
 const config = require('./config');
 
 const app = express();
-const port = 3333;
 
-app.listen(port,(err)=>{
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(config.port,(err)=>{
     if(err){
         console.log(err);
     }else {
-        console.log('Server started on port: '+ port);
+        console.log('Server started on port: '+ config.port);
     }
 });

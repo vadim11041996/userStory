424b5813e8c3c916d3f98f6f301f4fa8bb726c52
vadim11041996 <vadim11041996@hotmail.com>
2018-06-08 17:26:09 +0300

    bcrypt added with metod for hashing

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 
 var Schema = mongoose.Schema;
 
@@ -6,6 +7,18 @@ var UserSchema = new Schema({
     name:String,
     username:{type:String, required:true, index:{unique:true}},
     password:{type:String, required:true, select:false}
+});
+
+UserSchema.pre('save',(next)=>{
+    var user = this;
+    if (!user.isModified('password')) return next();
+
+    bcrypt.hash(user.password, null, null, (err, hash)=>{
+        if(err) return next(err);
+
+        user.password = hash;
+        next();
+    })
 });
 
 module.exports = mongoose.model('User', UserSchema);
